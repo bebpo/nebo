a24c5c53cc7e6d4f452b11e9c08bb6efa561290e
bebpo <58513962+bebpo@users.noreply.github.com>
2019-12-04 22:34:29 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("651762324927021090")
+let channel =     client.channels.get("651883836631678979")
 setInterval(function() {
 channel.send(`pleas`);
 }, 30)
